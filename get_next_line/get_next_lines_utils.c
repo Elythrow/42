c381d66d3eda9b83eb4d21a116dd8f082593a928
guiguizz <hello@DESKTOP-2EN31GC>
2024-11-07 00:00:20 +0100

    plan

--- get_next_line/get_next_lines_utils.c
+++ get_next_line/get_next_lines_utils.c
@@ -11,3 +11,101 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return ((char *)(&str[i]));
+		i ++;
+	}
+	if (str[i] == (char)c)
+		return ((char *)(&str[i]));
+	return (NULL);
+}
+
+char	*ft_strdup(const char *str)
+{
+	unsigned int	i;
+	char			*dup;
+
+	i = 0;
+	i = ft_strlen(str);
+	dup = malloc(i + 1);
+	if (dup == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i ++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+int		ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i ++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, int len)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	if (start > ft_strlen(s))
+	{
+		sub = malloc(sizeof(char));
+		if (sub == NULL)
+			return (NULL);
+		*sub = '\0';
+		return (sub);
+	}
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	sub = malloc((len + 1) * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	while (i < len && s[start])
+	{
+		sub[i] = s[start];
+		start ++;
+		i ++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	int		i;
+
+	i = 0;
+	join = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (join == NULL)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		join[i] = s1[i];
+		i ++;
+	}
+	while (i < (ft_strlen(s1) + ft_strlen(s2)))
+	{
+		join[i] = s2[i - ft_strlen(s1)];
+		i ++;
+	}
+	join[i] = '\0';
+	return (join);
+}
