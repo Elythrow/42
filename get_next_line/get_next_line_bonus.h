d5c6adc1fb4a3becb069d9c80b0fd842912591b6
Guillaume Bazin <gbazin@made-f0Br8s10.clusters.42paris.fr>
2024-11-15 12:26:13 +0100

    bonus avec le .h

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 12:13:07 by gbazin            #+#    #+#             */
+/*   Updated: 2024/11/15 12:24:28 by gbazin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# elif BUFFER_SIZE < 0
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+# endif
+
+char	*get_next_line(int fd);
+char	*fill_line_buffer(int fd, char *left_c, char *buffer);
+char	*set_line(char *line_buffer);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(char *str);
+size_t	ft_strlen(char *str);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
