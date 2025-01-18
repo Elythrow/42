adfb4190c0eab12753fe7cf62bed0a09fdff370a
Guillaume Bazin <gbazin@bess-f4r5s8.clusters.42paris.fr>
2025-01-18 17:04:30 +0100

    part 1

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,31 @@
 /*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 16:07:14 by gbazin            #+#    #+#             */
-/*   Updated: 2025/01/10 16:07:16 by gbazin           ###   ########.fr       */
+/*   Updated: 2025/01/18 17:04:12 by gbazin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+}	t_node;
+
+typedef struct s_stack
+{
+	t_node	*top;
+	int		size;
+}	t_stack;
+
+t_stack	*create_stack(void);
+t_node	*create_node(int value);
+void	free_stack(t_stack *stack);
+int		push(t_stack *stack, int value);
+int		pop(t_stack *stack);
+
+#endif
