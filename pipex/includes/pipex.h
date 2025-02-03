52538dc37ebef430cfe5d75da513583fd235081d
Guillaume Bazin <gbazin@bess-f2r1s18.clusters.42paris.fr>
2025-02-03 18:09:27 +0100

    pipex

---
+++ pipex/includes/pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 15:15:49 by gbazin            #+#    #+#             */
+/*   Updated: 2025/02/03 15:25:37 by gbazin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/wait.h>
+
+
+#endif
