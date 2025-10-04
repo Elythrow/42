45433ecae0a5b4efce5c360a6dd66523186b39cd
Guillaume Bazin <gbazin@f4r10s2.clusters.42paris.fr>
2025-10-04 21:04:21 +0200

    .

---
+++ cube/includes/cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 12:15:42 by gbazin            #+#    #+#             */
+/*   Updated: 2025/10/04 20:59:23 by gbazin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <math.h>
+# include "libft.h"
+# include "mlx.h"
+
+#endif
