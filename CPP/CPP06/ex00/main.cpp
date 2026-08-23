1ae75939aa96f8be7f36ca14d6f48d7243c8a467
gbazin <gbazin@f4r5s11.paris.42.school>
2026-08-23 20:33:58 +0200

    .

--- CPP/CPP06/ex00/main.cpp
+++ CPP/CPP06/ex00/main.cpp
@@ -6,56 +6,56 @@
 /*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 21:43:01 by gbazin            #+#    #+#             */
-/*   Updated: 2026/08/22 21:50:32 by gbazin           ###   ########.fr       */
+/*   Updated: 2026/08/23 18:43:30 by gbazin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-// void runTest(const std::string& input) {
-// 	std::cout << input << std::endl;
-// 	ScalarConverter::convert(input);
-// 	std::cout << std::endl;
-// }
-
-// int main(void)
-// {
-// 	runTest("a");
-// 	runTest("Z");
-// 	runTest("!");
-// 	runTest("7");
-// 	runTest("0");
-// 	runTest("42");
-// 	runTest("-42");
-// 	runTest("127");
-// 	runTest("128");
-// 	runTest("2147483647");
-// 	runTest("-2147483648");
-// 	runTest("2147483648");
-// 	runTest("-2147483649");
-// 	runTest("42.0f");
-// 	runTest("3.14f");
-// 	runTest("-0.5f");
-// 	runTest("42.0");
-// 	runTest("3.1415926535");
-// 	runTest("-0.0000001");
-// 	runTest("340282346638528859811704183484516925440.0f");
-// 	runTest("1e39f");
-// 	runTest("1e309");
-// 	runTest("nan");
-// 	runTest("nanf");
-// 	runTest("inf");
-// 	runTest("+inf");
-// 	runTest("-inf");
-// 	runTest("+inff");
-// 	runTest("-inff");
-// }
-
-int main(int ac, char **av) {
-	if (ac != 2) {
-		std::cerr << "Error! Usage: convert <value>" << std::endl;
-		return (EXIT_FAILURE);
-	}
-	ScalarConverter::convert(av[1]);
-	return (EXIT_SUCCESS);
+void runTest(const std::string& input) {
+	std::cout << input << std::endl;
+	ScalarConverter::convert(input);
+	std::cout << std::endl;
 }
+
+int main(void)
+{
+	runTest("a");
+	runTest("Z");
+	runTest("!");
+	runTest("7");
+	runTest("0");
+	runTest("42");
+	runTest("-42");
+	runTest("127");
+	runTest("128");
+	runTest("2147483647");
+	runTest("-2147483648");
+	runTest("2147483648");
+	runTest("-2147483649");
+	runTest("42.0f");
+	runTest("3.14f");
+	runTest("-0.5f");
+	runTest("42.0");
+	runTest("3.1415926535");
+	runTest("-0.0000001");
+	runTest("340282346638528859811704183484516925440.0f");
+	runTest("1e39f");
+	runTest("1e309");
+	runTest("nan");
+	runTest("nanf");
+	runTest("inf");
+	runTest("+inf");
+	runTest("-inf");
+	runTest("+inff");
+	runTest("-inff");
+}
+
+// int main(int ac, char **av) {
+// 	if (ac != 2) {
+// 		std::cerr << "Error! Usage: convert <value>" << std::endl;
+// 		return (EXIT_FAILURE);
+// 	}
+// 	ScalarConverter::convert(av[1]);
+// 	return (EXIT_SUCCESS);
+// }
--- CPP/CPP06/ex01/main.cpp
+++ CPP/CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 20:00:40 by gbazin            #+#    #+#             */
-/*   Updated: 2026/08/22 20:37:02 by gbazin           ###   ########.fr       */
+/*   Updated: 2026/08/23 18:41:21 by gbazin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,21 @@ int main(void)
 	std::cout << "Original pointer: " << &d << std::endl;
 	std::cout << "Raw value: " << raw << std::endl;
 	std::cout << "Recovered pointer: " << recovered << std::endl;
+	if (recovered == &d) {
+		std::cout << "Match, value = " << raw << std::endl;
+	} else {
+		std::cout << "Different"<< std::endl;
+	}
 
 	d.id = 1;
 	d.name = "Test";
 	std::cout << "Data: " << d.id << " " << d.name << std::endl;
 	raw = Serializer::serialize(&d);
 	std::cout << "Raw: " << Serializer::deserialize(raw)->id << " " << Serializer::deserialize(raw)->name << std::endl;	
+	if (Serializer::deserialize(raw)->id == d.id && Serializer::deserialize(raw)->name == d.name) {
+		std::cout << "Match, value = " << Serializer::deserialize(raw)->id << " " << Serializer::deserialize(raw)->name << std::endl;
+	} else {
+		std::cout << "Different"<< std::endl;
+	}
 	return (0);
 }
