f8eda78932e179709ef4bdcb43a7303988433f95
gbazin <gbazin@student.42.fr>
2026-08-28 21:53:21 +0200

    .

--- CPP/CPP08/ex01/Span.cpp
+++ CPP/CPP08/ex01/Span.cpp
@@ -6,7 +6,24 @@
 /*   By: gbazin <gbazin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 06:21:51 by gbazin            #+#    #+#             */
-/*   Updated: 2026/08/28 06:21:51 by gbazin           ###   ########.fr       */
+/*   Updated: 2026/08/28 21:52:51 by gbazin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+
+Span::Span(unsigned int n);
+Span::Span(const Span &src);
+Span &Span::operator=(const Span &rhs);
+~Span();
+
+
+
+
+
+unsigned int _n;
+std::vector<int> _numbers;
+
+void	addNumber(int n);
+int		shortestSpan();
+int		longestSpan();
